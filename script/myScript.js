f1df95b5bb3784a1e8e1f3a851c15636e799eedf
mike treml <miketreml@mac.com>
2020-10-13 14:21:35 -0500

    fixed layout

--- script/myScript.js
+++ script/myScript.js
@@ -14,10 +14,3 @@ function showInfo(data, tabletop) {
     '</div>');
     }
     }
-
-    <div class="card-header">Header</div>
-    <div class="card-body">
-      
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-  </div>
